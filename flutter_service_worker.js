d2f73d6b6b1e3cdba3bf9d6ed873f9300b2492c7
ryudai <>
2025-01-19 04:29:52 +0900

    Deploy with updated base href and start_url

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "4bddc55fbd5eff17d2a0307e067d1c19",
+const RESOURCES = {"flutter_bootstrap.js": "c4e8e1fff2afb3c42d9cab766a3aafa8",
 "version.json": "815cdc9dd45f86cb16261d7f71e3dea4",
 "index.html": "f556a38f30bcf0699a3dc84dec24d99e",
 "/": "f556a38f30bcf0699a3dc84dec24d99e",
